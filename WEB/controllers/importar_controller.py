641e9c3e264df3012bf2ca715378db11c9c7f6a4
Anderson Jimenez <asjimenez@intercolombia.com>
2025-10-29 14:23:31 -0500

    b

--- WEB/controllers/importar_controller.py
+++ WEB/controllers/importar_controller.py
@@ -3,6 +3,12 @@ import subprocess
 from utils.cli import build_cmd
 import os
 
+# Ruta del proyecto AutoADA (un nivel arriba)
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+AUTOADA_DIR = os.path.abspath(os.path.join(BASE_DIR, "..", "..", "AutoADA"))
+
+
+
 def ejecutar_importar(empresa: str):
     """
     Lógica simple para ejecutar el script importar_all con la empresa indicada.
@@ -12,7 +18,7 @@ def ejecutar_importar(empresa: str):
     cmd = build_cmd("scripts.importar_all", ["itco1sca01", empresa, "sca,hsh,ods", "--usecase", "buscar_keys"])
 
     try:
-        result = subprocess.run(cmd, capture_output=True, text=True)
+        result = subprocess.run(cmd, capture_output=True, text=True, cwd=AUTOADA_DIR)
         output = result.stdout + "\n" + result.stderr
         return f"Comando ejecutado: {' '.join(cmd)}\n\nSalida:\n{output}"
     except Exception as e:
